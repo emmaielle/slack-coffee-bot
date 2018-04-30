6b11e03d306266c09dec3df17bb5449ead61bbb1
Moira Lasserre <moira.lasserre@gmail.com>
2018-04-30 19:13:37 -0300

    initial bot commit

---
+++ skills/coffee_related.js
@@ -0,0 +1,20 @@
+var debug = require('debug')('botkit:channel_join');
+
+module.exports = function(controller) {
+
+    controller.hears(['coffee', 'cafe', 'café', 'Coffee', 'Coffee?', 'coffee?', '*coffee*', 'sup', 'give me coffee',
+    'Give me coffee'], 
+    ['direct_mention', 'mention', 'direct_message'], (bot, message) => {
+        bot.reply(message, 'beep bop...');
+        let chosen = 'no one';
+        let users = bot.users;
+        const random = Math.random();
+        // console.log(random);
+        let index = Math.floor(random*users.length);
+        // console.log(index);
+        chosen = users[index];
+        console.log(chosen);
+        bot.reply(message, `<@${chosen}> your turn to make coffee!`);
+    })
+
+}
